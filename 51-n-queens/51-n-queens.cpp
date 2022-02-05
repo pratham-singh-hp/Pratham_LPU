35d5f68dc70b72c337f08cf48a5d647a170b26fc
pratham-singh-hp <62982194+pratham-singh-hp@users.noreply.github.com>
2022-02-05 16:37:41 +0530

    Time: 4 ms (92.45%), Space: 7.2 MB (87.23%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+  void solve(int col,int n,vector<vector<string>> &ans,vector<string> &board,vector<bool> &back,vector<bool> &upperdiagonal,vector<bool> &lowerdiagonal){
+        //base case
+        if(col == n){
+            ans.push_back(board);
+            return;
+        }
+        //traversing through every row 
+        for(int row=0;row<n;row++){
+            if(!back[row] and !upperdiagonal[n-1 + col - row] and !lowerdiagonal[col+row]){
+                board[row][col] = 'Q';
+                back[row] = 1;
+                upperdiagonal[n-1 + col - row] = 1;
+                lowerdiagonal[row+col] = 1;
+                solve(col+1,n,ans,board,back,upperdiagonal,lowerdiagonal);
+                board[row][col] = '.';
+                back[row] = 0;
+                upperdiagonal[n-1 + col - row] = 0;
+                lowerdiagonal[row+col] = 0;
+            }
+        }
+        
+    }
+    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i] = s;
+        }
+        //using containers instead of is safe function
+        //space optimized using bool vector instead of int
+        // vector<int> back(n,0);
+        // vector<int> upperdiagonal(2*n-1,0);
+        // vector<int> lowerdiagonal(2*n-1,0);
+        vector<bool> back(n,0);
+        vector<bool> upperdiagonal(2*n-1,0);
+        vector<bool> lowerdiagonal(2*n-1,0);
+        solve(0,n,ans,board,back,upperdiagonal,lowerdiagonal);
+        return ans;
+    }   
+    
+};
